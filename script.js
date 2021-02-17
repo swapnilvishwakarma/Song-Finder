c0002f5a0e9fa4c99cded6142ba0c5833c650c0a
Swapnil Vishwakarma <swapnil.vishwakarma18@siesgst.ac.in>
2021-02-17 12:44:02 +0530

    Async code and promises

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const data = fetch('https://itunes.apple.com/search?');
+// const data = fetch('https://itunes.apple.com/search?');
 
-console.log(data);
+// console.log(data);
 
-setTimeout(() => {
-    console.log(data);
-}, 2000);
+// setTimeout(() => {
+//     console.log(data);
+// }, 2000);
+
+// JS code is asynchronus => while the request is being fetched, code below it is not blocked.
+
+const url = 'https://itunes.apple.com/search?term=beyounce';
+
+fetch(url, {mode: 'no-cors'})
+.then( (response) => response.json())
+.then((data) => {
+    console.log(data.results);
+})
+.catch(error => console.log('Request failed: ', error))
