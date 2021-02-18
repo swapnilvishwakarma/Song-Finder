b03a2c21c599580eac873a269ab360d6f4920280
Swapnil Vishwakarma <swapnil.vishwakarma18@siesgst.ac.in>
2021-02-18 23:36:31 +0530

    Audio previews, capture and bubbling

--- script.js
+++ script.js
@@ -24,40 +24,49 @@ const updateTerm = () => {
 
         const url = `https://itunes.apple.com/search?limit=15&media=music&term=${term}`
 
-fetch(url)
-.then( (response) => response.json() )
-.then((data) => {
-    // console.log(data.results);
-    const artists = data.results;
-    return artists.map(result => {
-        const article = document.createElement('article'),
-        artist = document.createElement('p'),
-        song = document.createElement('p'),
-        img = document.createElement('img'),
-        audio = document.createElement('audio'),
-        audioSource = document.createElement('source')
+        fetch(url)
+        .then( (response) => response.json() )
+        .then((data) => {
+            // console.log(data.results);
+            const artists = data.results;
+            return artists.map(result => {
+                const article = document.createElement('article'),
+                artist = document.createElement('p'),
+                song = document.createElement('p'),
+                img = document.createElement('img'),
+                audio = document.createElement('audio'),
+                audioSource = document.createElement('source')
 
-        artist.innerHTML = result.artistName
-        song.innerHTML = result.trackName
-        img.src = result.artworkUrl100
-        audioSource.src = result.previewUrl
-        audio.setAttribute('controls', '')
+                artist.innerHTML = result.artistName
+                song.innerHTML = result.trackName
+                img.src = result.artworkUrl100
+                audioSource.src = result.previewUrl
+                audio.setAttribute('controls', '')
 
-        article.appendChild(img)
-        article.appendChild(artist)
-        article.appendChild(song)
-        article.appendChild(audio)
-        article.appendChild(audioSource)
+                article.appendChild(img)
+                article.appendChild(artist)
+                article.appendChild(song)
+                article.appendChild(audio)
+                article.appendChild(audioSource)
 
-        songContainer.appendChild(article)
+                songContainer.appendChild(article)
 
-        // console.log(result)
-    })
-})
-.catch(error => console.log('Request failed: ', error))
-    }
+                // console.log(result)
+            })
+        })
+        .catch(error => console.log('Request failed: ', error))
+            }
 }
 
 
 const searchBtn = document.querySelector('button')
 searchBtn.addEventListener('click', updateTerm)
+
+document.addEventListener('play', event => {
+    const audio = document.getElementsByTagName('audio');
+    for (let i = 0; i < audio.length; i++) {
+        if ( audio[i] != event.target) {
+            audio[i].pause();
+        }
+    }
+}, true)
