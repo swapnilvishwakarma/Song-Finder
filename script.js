7011dceee6ec5eed698c5a268f4b13bb50e63263
Swapnil Vishwakarma <swapnil.vishwakarma18@siesgst.ac.in>
2021-02-18 22:54:43 +0530

    Map through results

--- script.js
+++ script.js
@@ -13,6 +13,33 @@ const url = 'https://itunes.apple.com/search?term=beyonce'
 fetch(url)
 .then( (response) => response.json() )
 .then((data) => {
-    console.log(data.results);
+    // console.log(data.results);
+    const artists = data.results;
+    return artists.map(result => {
+        const songContainer = document.getElementById('songs')
+
+        const article = document.createElement('article'),
+        artist = document.createElement('p'),
+        song = document.createElement('p'),
+        img = document.createElement('img'),
+        audio = document.createElement('audio'),
+        audioSource = document.createElement('source')
+
+        artist.innerHTML = result.artistName
+        song.innerHTML = result.trackName
+        img.src = result.artworkUrl100
+        audioSource.src = result.previewUrl
+        audio.setAttribute('controls', '')
+
+        article.appendChild(img)
+        article.appendChild(artist)
+        article.appendChild(song)
+        article.appendChild(audio)
+        article.appendChild(audioSource)
+
+        songContainer.appendChild(article)
+
+        // console.log(result)
+    })
 })
 .catch(error => console.log('Request failed: ', error))
